17294f41ecde2fe90acb5fcdc55c44556d12398e
Yaasir024 <yope4ever@gmail.com>
2023-06-16 11:31:34 +0100

    /

--- src/components/home/Section3.vue
+++ src/components/home/Section3.vue
@@ -8,24 +8,24 @@
         <div class="flex flex-wrap w-full">
             <div class="flex-full sm:flex-50% xl:flex-25% pt-[64px] px-[20px] xs:px-[45px] sm:px-[64px] 2xl:px-[64px] border-t border-r border-[#545440] pb-12 md:h-[400px] flex flex-col">
                 <h3 class="text-[80px] leading-[81px] tracking-[0.04em] font-medium font-manuka text-rm-green">2021</h3>
-                <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light">r17 Launch Date</h3>
+                <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light capitalize">r17 Launch Date</h3>
             </div>
             <div class="flex-full sm:flex-50% xl:flex-25% pt-[64px] px-[20px] xs:px-[45px] sm:px-[64px] 2xl:px-[64px] border-t border-r border-[#545440] pb-12 md:h-[400px]  flex justify-end sm:justify-start">
                 <div class="flex flex-col">
                     <h3 class="text-[80px] leading-[81px] tracking-[0.04em] font-medium font-manuka text-rm-green"><span class="font-semibold text-[54px] font-dm-sans">$</span>35M+</h3>
-                    <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light">Active Fund size</h3>
+                    <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light capitalize">Active Fund size</h3>
 
                 </div>
             </div>
             <div class="flex-full sm:flex-50% xl:flex-25% pt-[64px] px-[20px] xs:px-[45px] sm:px-[64px] 2xl:px-[64px] border-t border-r border-[#545440] pb-12 md:h-[400px] flex flex-col ">
                 <h3 class="text-[80px] leading-[81px] tracking-[0.04em] font-medium font-manuka text-rm-green">10+</h3>
-                <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light">Active Verticals</h3>
+                <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light capitalize">Active Verticals</h3>
             </div>
             <div class="flex-full sm:flex-50% xl:flex-25% pt-[64px] px-[20px] xs:px-[45px] sm:px-[64px] 2xl:px-[64px] border-t border-[#545440] border-b xs:border-b-0 pb-12 md:h-[400px] flex justify-end sm:justify-start">
                 <div class="flex flex-col">
 
                     <h3 class="text-[80px] leading-[81px] tracking-[0.04em] font-medium font-manuka text-rm-green">17+</h3>
-                    <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light">Ventures funded </h3>
+                    <h3 class="text-[28px] leading-[36px] tracking-[0.01em] font-medium mt-[8px] text-rm-light capitalize">Ventures funded </h3>
                 </div>
             </div>
         </div>
